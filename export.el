514b606f2630eaa1d41ae12a15aea48adc19180b
Igor Zhirkov <sayon@Igors-MBP.lan>
2024-09-25 00:43:57 +0200

    new post

--- export.el
+++ export.el
@@ -1,7 +1,4 @@
 (require 'ox-publish)
-(require 'ox-html)
-(load-file "./ox-rss.el")
-
 
 (setq pubdir "~/public_html")
 (setq basedir "~/repos/blog")
@@ -22,15 +19,16 @@
   (if filename
       (progn
         (when-let ((contents (org-file-contents filename :noerror)))
-                   (with-temp-buffer
-                     (message filename)
-                     (insert contents)
-                     (org-collect-keywords keywords unique directory))
-                   ))))
+          (with-temp-buffer
+            (message filename)
+            (insert contents)
+            (org-collect-keywords keywords unique directory))
+          ))))
 
 (defun my/org-publish-find-excerpt-or-empty (filename) ""
-       (let ((lst (my/org-collect-keywords '("excerpt") filename)))
-         (if lst (second (first lst)) "" )))
+  (let* ((keyword-list (my/org-collect-keywords '("excerpt") filename))
+         (excerpt (and keyword-list (nth 1 (car keyword-list)))))
+    (or excerpt "")))
 
 (defun my/org-sitemap-date-entry-format (entry style project) ""
        (let ((title (org-publish-find-title entry project)))
@@ -45,19 +43,7 @@
                     (org-macro-escape-arguments
                      (my/org-publish-find-excerpt-or-empty (org-publish--expand-file-name entry project)))
                     )))))
-;; (defun my/org-sitemap-date-entry-format (entry style project) ""
-;;        (let ((title (org-publish-find-title entry project)))
-;;          (if (= (length title) 0)
-;;              (format "*%s*" entry)
-;;            (concat
-;;             (format "{{{timestamp(%s)}}} [[file:%s][%s]] \n {{{div(%s)}}}"
-;;                     (format-time-string "%d-%m-%Y"
-;;                                         (org-publish-find-date entry project))
-;;                     entry
-;;                     title
-;;                     (org-macro-escape-arguments
-;;                      (my/org-publish-find-excerpt-or-empty (org-publish--expand-file-name entry project)))
-;;                     )))))
+
 
 (setq org-publish-project-alist
       (list
@@ -113,46 +99,16 @@
              )
        (list "blog-static"
              :base-directory basedir
-             :base-extension "css\\|js\\|png\\|jpg\\|\\jpeg\\|gif\\|pdf\\|mp3\\|svg"
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
              :publishing-directory pubdir
              :excluded '("setup" "posts")
              :recursive t
              :publishing-function 'org-publish-attachment
              )
-(list "rss"
-     :base-directory "posts"
-     :base-extension "org"
-     ;; :rss-image-url "http://lumiere.ens.fr/~guerry/images/faces/15.png"
-     :html-link-home "https://rubber-duck-typing.com"
-     :html-link-use-abs-url t
-     :rss-extension "xml"
-     :publishing-directory pubdir
-     :publishing-function 'org-rss-publish-to-rss
-     :section-numbers nil
-     :exclude ".*"            ;; To exclude all files...
-     :include '("sitemap.org")   ;; ... except index.org.
-     :table-of-contents nil)
+       (list "blog-all" :components '("blog-posts" "blog-static" "blog"))
+       ))
 
-      (list "blog" :components '("blog-posts" "blog-static" "rss" "blog"))
-      ))
 
 (org-publish-all)
-
-(save-window-excursion (async-shell-command "rsync -avLK ~/public_html/ rdt:~/public_html"))
-
-;; <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
-;; <generator uri="https://www.gnu.org/software/emacs/" version="29">Emacs</generator>
-;; <link href="http://rubber-duck-typing.com/feed.xml" rel="self" type="application/atom+xml"/>
-;; <link href="https://rubber-duck-typing.com/" rel="alternate" type="text/html" hreflang="en"/>
-;; <updated>2019-11-17T20:22:37-06:00</updated>
-;; <id>https://www.rubber-duck-typing.com/</id>
-;; <title type="html">Ted Kaminski</title>
-;; <subtitle>Ted Kaminski's blog - a software design book writing project.</subtitle>
-;; <author>
-;; <name>Ted Kaminski</name>
-;; <email>tedinski@cs.umn.edu</email>
-;; <uri/>
-;; </author>
-;; <entry>
-;; ...
-;; </entry>
+;;(org-publish-project "blog-all")
+;;(save-window-excursion (async-shell-command "rsync -avLK ~/public_html/ rdt:~/public_html"))
